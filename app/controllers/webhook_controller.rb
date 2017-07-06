d8d707f3be4bd6c30460f2aff7c638c826b18c9c
sogawa <s.ogawa055@gmail.com>
2017-07-06 06:58:50 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -39,7 +39,6 @@ class WebhookController < ApplicationController
     modified_text1 = output_text.gsub(/私/, "うち")
     modified_text2 = modified_text1.gsub(/。|です|ですよ|でした|だね|よね|？/,"")
     ram_text = modified_text2 + gobi[y].to_s + mark[x].to_s
-  end
     
     client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
     res = client.reply(replyToken, ram_text)
