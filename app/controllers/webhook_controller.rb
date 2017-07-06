632ddf24110b2ec752bb6215ce52dd9d4ef20aed
sogawa <s.ogawa055@gmail.com>
2017-07-06 02:50:36 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -18,10 +18,7 @@ class WebhookController < ApplicationController
     
    if user_words == "テスト" then
       @post = RamPost.offset(rand(RamPost.count)).first
-      $ram_text　= "テスト"
-      
-    client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
-    res = client.reply(replyToken, $ram_text)
+      ram_text　= @post
     
    else
     docomo_client = DocomoClient.new(api_key: ENV["DOCOMO_API_KEY"])
@@ -44,13 +41,13 @@ class WebhookController < ApplicationController
     y = rand(0..1)
     modified_text1 = output_text.gsub(/私/, "うち")
     modified_text2 = modified_text1.gsub(/。|です|ですよ|でした|だね|よね|？/,"")
-    $ram_text = modified_text2 + gobi[y].to_s + mark[x].to_s
-    
-    client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
-    res = client.reply(replyToken, $ram_text)
+    ram_text = modified_text2 + gobi[y].to_s + mark[x].to_s
+
     
    end
     
+    client = LineClient.new(CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
+    res = client.reply(replyToken, ram_text)
 
 
     if res.status == 200
