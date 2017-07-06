0647ec2de784afe4ba7791df021bc9ef3b00c515
sogawa <s.ogawa055@gmail.com>
2017-07-06 03:17:37 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -17,7 +17,7 @@ class WebhookController < ApplicationController
     user_words = event["message"]["text"]
     
    if user_words == "テスト" then
-      @post = RamPost.find(params[:words])
+      @post = RamPost.find(1)
       ram_text = @post
     
    else
